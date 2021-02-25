80268de6e5dbef5be8d27b887e69894e8e400ba9
Zhang Chi <zliming@outlook.com>
2021-02-25 14:05:00 +0800

    :building_construction: make designer as index

---
+++ src/Designer/Dmd.Designer/Pages/Index.razor.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dmd.Designer.Components.Canvas;
+
+namespace Dmd.Designer.Pages
+{
+    public partial class Index
+    {
+        private DmdCanvasComponent _dmdCanvasComponent;
+        private DmdCanvasContext _dmdCanvasContext;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await Task.Run(() =>
+            {
+                _dmdCanvasContext = _dmdCanvasComponent.Context();
+            });
+        }
+
+        private async Task AddEntityAsync()
+        {
+            await _dmdCanvasContext.AddClassComponentAsync(
+                "User",
+                new string[]
+                {
+                    "Id",
+                    "Name",
+                    "Sex",
+                    "Age"
+                },
+                new string[]
+                {
+                    "SetName()",
+                    "SetAge()"
+                },
+                new double[]
+                {
+                    100,
+                    100
+                });
+            await _dmdCanvasContext.AddClassComponentAsync(
+                "Product",
+                new string[]
+                {
+                    "Id",
+                    "Name",
+                    "Price"
+                },
+                new string[]
+                {
+                    "SetName()",
+                    "SetPrice()"
+                },
+                new double[]
+                {
+                    300,
+                    300
+                });
+        }
+    }
+}
