0e6c74fe8aa678a86ea79bdfc41066a37e8e8fa7
Zhang Chi <zliming@outlook.com>
2021-03-27 15:58:04 +0800

    :label: entity view model

---
+++ src/Dmd.Designer/Models/EntityModel.cs
@@ -0,0 +1,21 @@
+﻿using Dmd.SourceOptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dmd.Designer.Models
+{
+    public class EntityModel
+    {
+        public string Name { get; set; }
+
+        public IList<PropertyModel> Properties { get; set; }
+
+        public EntityModel()
+        {
+            Properties = new List<PropertyModel>();
+        }
+
+    }
+}
---
+++ src/Dmd.Designer/Models/PropertyModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dmd.Designer.Models
+{
+    public class PropertyModel
+    {
+        public string AccessLevel { get; set; }
+
+        public string Type { get; set; }
+
+        public string Name { get; set; }
+
+        public string GetAccessLevel { get; set; }
+
+        public string SetAccessLevel { get; set; }
+
+        // TODO: set value asynchronously https://stackoverflow.com/questions/6602244/how-to-call-an-async-method-from-a-getter-or-setter
+        private const string CollapseHide = "collapse";
+        private const string CollapseHiding = "collapsing";
+        private const string CollapseShowing = "collapsing show";
+        private const string CollapseShow = "collapse show";
+        public string CollapseStyle => Collapse ? CollapseShow : CollapseHide;
+        private const string CollapseHiddenIconStyle = "fa-angle-right";
+        private const string CollapseShowIconStyle = "fa-angle-down";
+        public string CollapseIconStyle => Collapse ? CollapseShowIconStyle : CollapseHiddenIconStyle;
+        public bool Collapse { get; set; }
+    }
+}
